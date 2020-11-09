adb78f09e37d59d6979dd816d9f902f00cf20a39
wo1fsea <wo1fsea@126.com>
2020-11-09 19:29:49 +0800

    del: debug run

--- build.sh
+++ build.sh
@@ -3,7 +3,3 @@ mkdir ./build
 cd ./build/ 
 cmake -DCMAKE_TOOLCHAIN_FILE="~/vcpkg/scripts/buildsystems/vcpkg.cmake" ../ 
 cmake --build .
-./lens/lens 0 &
-./lens/lens 1 &
-./lens/lens 2 &
-./lens/lens 3 &
